d8b8fc0ddfc5b798a20f20c6a9d0bbfdc9be3b2a
Derrick Schneider <derrick.schneider@opower.com>
2015-07-22 17:37:40 -0400

    Take arguments, clean up, etc.

--- get_all_prs/get_all_prs.sh
+++ get_all_prs/get_all_prs.sh
@@ -1,8 +1,17 @@
-# Various commands to query the number of PRs for a given repo or repos within an org
-curl -s 'https://github.va.opower.it/api/v3/search/issues?q=type:pr+user:chef+user:releng+state:open' | \
-  jq '.items | .[] | [.url, .title, .user.login, .updated_at] | @csv' | tr -d '"\\' | sort | \
-  awk -f ~/format-repo-output.awk
+#!/bin/bash
 
-curl -s "https://github.va.opower.it/api/v3/repos/opower/archmage/pulls" | \
-    jq '.[] | [.url, .title, .user.login, .updated_at] | @csv' | tr -d '"\\' | \
-    awk -f ~/format-repo-output.awk
+# Various commands to query the number of PRs for a given repo or repos within an org
+GH_QUERY="type:pr state:open"
+
+for query_term in "$@"
+do
+  if [[ $query_term =~ \/ ]]
+  then
+    GH_QUERY="$GH_QUERY repo:$query_term"
+  else
+    GH_QUERY="$GH_QUERY user:$query_term"
+  fi
+done
+
+curl -G -s 'https://github.va.opower.it/api/v3/search/issues' --data-urlencode "q=$GH_QUERY" --data-urlencode "per_page=100" | \
+   jq '.items | .[] | [.url, .title, .user.login, .updated_at] | @csv' | tr -d '"\\' | sort
