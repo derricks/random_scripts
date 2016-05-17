d443593176bcff767800f521277b341a7143fa83
Derrick Schneider <derrick.schneider@dschneider.opower.local>
2016-05-17 13:56:47 -0700

    Add remotes for completing push/pull/rebase/fetch. Clean up

--- bash_completions.sh
+++ bash_completions.sh
@@ -6,23 +6,33 @@ _ssh_complete() {
 
 complete -o bashdefault -o default -F _ssh_complete ssh
 
-# git commands
+# gets the set of branches in the current directory
 _branches_in_repo() {
-  if [[ $3 == "checkout" ]];
-  then
-    COMPREPLY=( $(git branch | tr -d " \*" | grep "^$2") )
-  fi
+  COMPREPLY=( $(git branch | tr -d " \*" | grep "^$2") )
+}
+
+# gets the set of remotes for tab-completes on
+# push, pull, rebase, fetch
+_remotes_in_repo() {
+  COMPREPLY=( $(git remote | grep $2) )
 }
 
 _git_complete() {
   # git sub commands, the case where the previous
   # word ("git") equals the command
+
+  # simple trick for seeing which commands might need remotes after them
+  remote_complete_commands=(push pull fetch rebase)
+
   if [[ $3 == $1 ]];
   then
     COMPREPLY=( $(git | awk -v regex="^ +[a-z]" '$0 ~ regex {print $1}' | grep "^$2") )
   elif [[ $3 == "checkout" ]];
-  then
-    _branches_in_repo $1 $2 $3
+    then
+      _branches_in_repo $1 $2 $3
+  elif [[ " ${remote_complete_commands[@]} " =~ $3 ]];
+    then
+      _remotes_in_repo $1 $2 $3
   fi
 }
 
