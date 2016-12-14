0d03047de73a5b4829a9b3e1b4472aa8a6451ccb
Derrick Schneider <derrick@Derrick.local>
2016-12-14 11:32:32 -0800

    Add merge to commands that get branch completions

--- bash_completions.sh
+++ bash_completions.sh
@@ -20,13 +20,14 @@ _remotes_in_repo() {
 _git_complete() {
   # simple trick for seeing which commands might need remotes after them
   remote_complete_commands=(push pull fetch rebase)
+  branch_complete_commands=(checkout merge)
 
   # git sub commands, the case where the previous
   # word ("git") equals the command
   if [[ $3 == $1 ]];
   then
     COMPREPLY=( $(git help -a | awk 'NF >= 1 {print $1; print $2}' | grep "^$2") )
-  elif [[ $3 == "checkout" ]];
+  elif [[ " ${branch_complete_commands[@]} " =~ $3 ]];
     then
       _branches_in_repo $1 $2 $3
   elif [[ " ${remote_complete_commands[@]} " =~ $3 ]];
@@ -48,3 +49,14 @@ _vagrant_complete() {
 }
 
 complete -o bashdefault -o default -F _vagrant_complete vagrant
+
+_packer_complete() {
+
+  # packer sub commands
+  if [[ $3 == $1 ]];
+  then
+    COMPREPLY=( $(packer | awk 'NF > 2 && /^[ ]+/ {print $1}' | grep "^$2") )
+  fi
+}
+
+complete -o bashdefault -o default -F _packer_complete packer
